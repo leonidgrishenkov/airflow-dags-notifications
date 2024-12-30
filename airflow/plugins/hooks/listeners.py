5e7a817ed3413ec199e65b3567b4ad17e01e8305
Grishenkov Leonid <grishenkovleonid@gmail.com>
2024-12-30 23:36:10 +0300

    feat(listeners): Add on_dag_run_success listener; Refactor messages

--- airflow/plugins/hooks/listeners.py
+++ airflow/plugins/hooks/listeners.py
@@ -14,6 +14,7 @@ from airflow.listeners import hookimpl
 from airflow.models.taskinstance import TaskInstance
 from airflow.utils.session import create_session
 from airflow.utils.state import TaskInstanceState
+from airflow.utils.types import DagRunType
 
 if TYPE_CHECKING:
     from airflow.models.dagrun import DagRun
@@ -81,7 +82,10 @@ def on_dag_run_failed(dag_run: DagRun, msg: str):
     airflow_instance = socket.gethostname()  # TODO: is it correct?
     dag_id = dag_run.dag_id
     run_id = dag_run.run_id
+    run_type = dag_run.run_type
     owner = dag_run.get_dag().owner
+    data_interval_start = dag_run.data_interval_start.strftime("%Y-%m-%d %H:%M:%S UTC")
+    data_interval_end = dag_run.data_interval_end.strftime("%Y-%m-%d %H:%M:%S UTC")
 
     # TODO: how to get correct webserver url?
     # base_url = conf.get("webserver", "base_url")
@@ -89,13 +93,53 @@ def on_dag_run_failed(dag_run: DagRun, msg: str):
     dag_run_url = f"{base_url}/dags/{dag_id}/grid?search={dag_id}&dag_run_id={quote_plus(run_id)}"
 
     message = (
-        "Dag failed! 🙈\n\n"
-        f"DAG: {dag_id}\n"
-        f"Run id: {run_id}\n"
-        f"Owner: {owner}\n"
-        f"Airflow instance: {airflow_instance}\n"
-        f"Responsible: {_map_responsible(owner)}\n\n"
+        f"DAG run failed! 🙈\n\n"
+        f"<b>DAG:</b> {dag_id}\n"
+        f"<b>Run ID:</b> {run_id}\n"
+        f"<b>Run type:</b> {run_type}\n"
+        f"<b>Data interval start:</b> {data_interval_start}\n"
+        f"<b>Data interval end:</b> {data_interval_end}\n"
+        f"<b>Owner:</b> {owner}\n"
+        f"<b>Airflow instance:</b> {airflow_instance}\n"
+        f"<b>Responsible:</b> {_map_responsible(owner)}\n\n"
         f'<a href="{dag_run_url}">Open DAG Run</a>'
     )
 
     TelegramNotifierBot().send_message(message, TELEGRAM_CHAT_ID)
+
+@hookimpl
+def on_dag_run_success(dag_run: DagRun, msg: str):
+    """
+    This method is called when dag run state changes to SUCCESS.
+    """
+    TELEGRAM_CHAT_ID = 196255068
+
+    # We will send message only if DAG was triggered manually
+    if dag_run.run_type == DagRunType.MANUAL:
+
+        airflow_instance = socket.gethostname()
+        dag_id = dag_run.dag_id
+        run_id = dag_run.run_id
+        owner = dag_run.get_dag().owner
+        start_date = dag_run.start_date.strftime("%Y-%m-%d %H:%M:%S UTC")
+        end_date = dag_run.end_date.strftime("%Y-%m-%d %H:%M:%S UTC")
+
+        base_url = "http://leonidgrishenkov.com:8080"
+        dag_run_url = f"{base_url}/dags/{dag_id}/grid?search={dag_id}&dag_run_id={quote_plus(run_id)}"
+
+        message = (
+            f"Manually triggered DAG completed successfully! 🍻\n\n"
+            f"<b>DAG:</b> {dag_id}\n"
+            f"<b>Run ID:</b> {run_id}\n"
+            f"<b>Started:</b> {start_date}\n"
+            f"<b>Ended:</b> {end_date}\n"
+            f"<b>Owner:</b> {owner}\n"
+            f"<b>Airflow instance:</b> {airflow_instance}\n"
+            f"<b>Responsible:</b> {_map_responsible(owner)}\n\n"
+            f'<a href="{dag_run_url}">Open DAG Run</a>'
+        )
+
+        TelegramNotifierBot().send_message(message, TELEGRAM_CHAT_ID)
+
+    else:
+        pass
